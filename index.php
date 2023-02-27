71d3f9e3e7a896d244d8ef7672281fce4aa88f68
ifanhananwijianto <ifanhanan22@gmail.com>
2023-02-27 16:50:26 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portofolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
+          
+  <body class="bg-secondary">
+  <div class="container mt-5 mb-5">
+  <div class="card text-center shadow">
+  <div class="card-body px-4 py-4">
+    <img src="ifan.jpg" class="rounded-circle border border-3" width="200">
+    <h1 class="mb-0">Ifan Hanan Wijianto</h1>
+    <span class="fw-light d-block mb-3" >Web Developer</span>
+    <p class="card-text">Saya mahasiswa aktif Universitas Sain dan teknologi Komputer Semarang 
+        Falkutas Studi Akademik Program studi S1 Tekni Informatika</p>
+  </div>
+  <div class="card-footer bg-white fs-2">
+    <a href="http://t.me/ifanhanan"><i class="fa-brands fa-telegram"></i></a>
+    <a href="https://instagram.com/ifanhnn?igshid=Mzc0YWU1OWY="><i class="fa-brands fa-square-instagram"></i></a>
+    <a href="https://wa.me/6288233205623"><i class="fa-brands fa-square-whatsapp"></i></a>
+
+</div>
+</div>
+
+<div class="card shadow mb-5">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+        <div class="row">
+            <div class="col-4">Email</div>
+            <div class="col-8 text-end text-muted">ifanhanan22@gmail.com</div>
+        </div>
+    </li>
+    <li class="list-group-item">
+        <div class="row">
+            <div class="col-4">Github</div>
+            <div class="col-8 text-end text-muted">https://github.com/IfanHananWijianto</div>
+        </div>
+    </li>
+    <li class="list-group-item">
+        <div class="row">
+            <div class="col-4">Phone</div>
+            <div class="col-8 text-end text-muted">088233205623</div>
+        </div>
+    </li>
+  </ul>
+</div>
+
+<div class="card shadow mb-5" >
+    <div class="card-header bg-light fw-bold">Programing Language</div>
+    <div class="card-body">
+        <div class="row row-cols-5 fs-1 text-secondary text-center">
+            <div class="col"><i class="devicon-html5-plain"></i></div>
+            <div class="col"><i class="devicon-css3-plain"></i></div>
+            <div class="col"><i class="devicon-php-plain"></i></div>
+            <div class="col"><i class="devicon-javascript-plain"></i></div>
+            <div class="col"><i class="devicon-java-plain"></i></div>
+            <div class="col"><i class="devicon-bootstrap-plain"></i></div>
+            <div class="col"><i class="devicon-mysql-plain"></i></div>
+          
+        </div>
+    </div>
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
